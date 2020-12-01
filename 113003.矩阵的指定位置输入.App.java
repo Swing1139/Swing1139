222fa06b328ab5cd5d690664aa1fbd47b0b36b7a
Swing1139 <74773761+Swing1139@users.noreply.github.com>
2020-12-01 20:00:36 +0800

    矩阵的指定位置输入.App
    矩阵的指定位置输入.App

---
+++ 113003.矩阵的指定位置输入.App.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class App implements R,S {
+	public static void main(String[] args) {
+    int array2d[][]=new int [10][10];
+    try (Scanner scanner = new Scanner(System.in)) {
+		int row=scanner.nextInt();
+		int column=scanner.nextInt();
+		int value=scanner.nextInt();
+		
+		array2d[row][column]=value;
+	}
+    
+//输出
+    for (int rowrow = 0; rowrow<array2d.length;rowrow++) {
+    	 for (int columnnn = 0; columnnn<array2d.length;columnnn++) {
+    		 System.out.print(array2d[rowrow][columnnn]+" ");
+    	 }
+    	 System.out.println();
+    }
+	}
+}
